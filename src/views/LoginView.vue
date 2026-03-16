ac1205da33798b841f0738c5f8710153f3d6423e
Mzrbt <jabetmarin07@gmail.com>
2026-03-16 01:16:26 +0100

    Config for tailwind & Beginning of the login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,1 +1,12 @@
-<template><p>Login</p></template>
+<script>
+
+</script>
+
+<template>
+    <div class="flex flex-col pt-20 items-center">
+        <h1 class="text-5xl">Connexion</h1>
+        <form action="" class="pt-15">
+            <input class="border" type="email" placeholder="E-mail">
+        </form>
+    </div>
+</template>
